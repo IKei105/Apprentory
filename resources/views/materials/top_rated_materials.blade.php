c9242a8703a728c65261f4ec08c51b9d971e8208
IKei105 <natsuki.kei105@gmail.com>
2025-01-08 19:41:25 +0900

    推奨教材用のサイトのファイルを作成しました

--- resources/views/materials/top_rated_materials.blade.php
+++ resources/views/materials/top_rated_materials.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="//demo.productionready.io/main.css" />
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> -->
     <link rel="stylesheet" href="top_rated_materials.css">
-    <title>ログイン</title>
+    <link rel="stylesheet" href="menu-select.css">
+    <title>教材一覧ページ</title>
 </head>
 <body>
     <div class="menu-select">
@@ -15,7 +15,9 @@
             <button class="new-button">新着</button>
             <button class="high-rated-button">高評価</button>
         </div>
-        <div class="filter"><p>絞り込み</p></div>
+        <div class="filter">
+            <p>絞り込み</p>
+        </div>
     </div>
     <div class="high-rated-materials">
         <h1 class="high-rated-title">評価の高い教材</h1>
