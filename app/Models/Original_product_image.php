beea4cbf3917ed114abb6308d95cd1b475e042d6
T-kakinoki <kknk.courage@gmail.com>
2025-01-15 12:03:35 +0000

    オリプロ投稿ページその他編集

--- app/Models/Original_product_image.php
+++ app/Models/Original_product_image.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Original_product_image extends Model
 {
     public $timestamps = false;
+
+    protected $fillable = ['original_product_id', 'image_dir'];
+    
+    public function product()
+    {
+        return $this->belongsTo(Original_product::class);
+    }
 }
--- app/Models/Technologie.php
+++ app/Models/Technologie.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Technologie extends Model
 {
+    protected $fillable = ['name'];
     public $timestamps = false;
+
+    // プロダクトとの多対多リレーション
+    public function products()
+    {
+        return $this->belongsToMany(Original_product::class, 'original_product_technologie_tag');
+    }
 }
