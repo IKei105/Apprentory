7bce72b25a9ee652ca4e18d006f8bd9b386be68b
IKei105 <natsuki.kei105@gmail.com>
2025-01-22 19:57:09 +0900

    分類した時のjsコードを変更しました

--- public/js/materials_index.js
+++ public/js/materials_index.js
@@ -7,42 +7,28 @@ const recommendedMaterialsAll = document.getElementById('recommended_materials_a
 const highRatedMaterialsAll = document.getElementById('high-rated-materials-all'); //もっと見る高評価教材
 const latestMaterialsAll = document.getElementById('latest-materials-all'); //もっと見る新着教材
 
-// 推奨教材ボタン
+// すべての教材を非表示にする関数
+function hideAllMaterials() {
+    recommendedMaterials.classList.add('hidden');
+    highRatedMaterials.classList.add('hidden');
+    latestMaterials.classList.add('hidden');
+    recommendedMaterialsAll.classList.add('hidden');
+    highRatedMaterialsAll.classList.add('hidden');
+    latestMaterialsAll.classList.add('hidden');
+}
+
 document.getElementById('recommended-button').addEventListener('click', () => {
-    // 見えるようにするやつ
-    recommendedMaterialsAll.classList.remove('hidden');
-    // 見えなくするやつ
-    recommendedMaterials.classList.add('hidden');
-    highRatedMaterials.classList.add('hidden');
-    latestMaterials.classList.add('hidden');
-    
-    highRatedMaterialsAll.classList.add('hidden');
-    latestMaterialsAll.classList.add('hidden');
-
+    hideAllMaterials(); // 全て非表示
+    recommendedMaterialsAll.classList.remove('hidden'); // 推奨教材を表示
 });
 
-// 新着ボタン
 document.getElementById('new-button').addEventListener('click', () => {
-    // 見えるようにするやつ
-    latestMaterialsAll.classList.remove('hidden');
-    // 見えなくするやつ
-    recommendedMaterials.classList.add('hidden');
-    highRatedMaterials.classList.add('hidden');
-    latestMaterials.classList.add('hidden');
-
-    recommendedMaterialsAll.classList.add('hidden');
-    highRatedMaterialsAll.classList.add('hidden');
+    hideAllMaterials(); // 全て非表示
+    latestMaterialsAll.classList.remove('hidden'); // 新着教材を表示
 });
 
-// 評価の高い教材ボタン
 document.getElementById('high-rated-button').addEventListener('click', () => {
-    // 見えるようにするやつ
-    highRatedMaterialsAll.classList.remove('hidden');
-    // 見えなくするやつ
-    recommendedMaterials.classList.add('hidden');
-    highRatedMaterials.classList.add('hidden');
-    latestMaterials.classList.add('hidden');
-
-    recommendedMaterialsAll.classList.add('hidden');
-    latestMaterialsAll.classList.add('hidden');
+    hideAllMaterials(); // 全て非表示
+    highRatedMaterialsAll.classList.remove('hidden'); // 高評価教材を表示
 });
+
