340b8a396bff91aba6d29e11e1dfc94b9b2ca6f2
IKei105 <natsuki.kei105@gmail.com>
2025-05-22 15:25:26 +0900

    オリプロ周りの処理をサービスに移行しました

--- app/Models/Original_product_technologie_tag.php
+++ app/Models/Original_product_technologie_tag.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Original_product_technologie_tag extends Model
 {
     public $timestamps = false; // タイムスタンプを無効化
+
+    protected $fillable = [
+        'original_product_id',
+        'technologie_id',
+        ];
+
     public function technologies()
     {
         return $this->belongsToMany(Technologie::class, 'original_product_technologie_tags', 'original_product_id', 'technologie_id');
